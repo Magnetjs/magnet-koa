24e6e0af41cad74f48e62a332bde2c84cc9ebb06
Kieve Chua <kievechua@gmail.com>
2017-02-25 12:52:20 +0800

    Conver to typescript

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { Module } from 'magnet-core/module'
+import Koa from 'koa'
+
+import defaultConfig from './config/koa'
+
+export default class KoaMagnet extends Module {
+  async setup () {
+    const config = this.setConfig('koa', defaultConfig)
+
+    // Setup Koa
+    this.app.koa = new Koa()
+    this.app.application = this.app.koa // Backward compatible, maybe remove later
+    this.app.koa.on('error', (err) => {
+      this.log.error(err)
+    })
+  }
+}
---
+++ src/start.ts
@@ -0,0 +1,17 @@
+import { Module } from 'magnet-core/module'
+import defaultConfig from './config/koa'
+
+export default class KoaStart extends Module {
+  async setup () {
+    try {
+      const config = this.setConfig('koa', defaultConfig)
+
+      this.app.koaServer = this.app.koa.listen(config.port)
+      this.app.server = this.app.koaServer
+      this.log.info(`Server started at port ${config.port}`)
+    } catch (err) {
+      this.log.error(err)
+      throw err
+    }
+  }
+}
